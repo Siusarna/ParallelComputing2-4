de3a5f812a04367c1a99dfaf27aa99d09dea5ef9
Siusarna <charodeyrap@gmail.com>
2021-12-22 17:15:22 +0200

    add comments

--- Lab1/SkipListLockFree.cs
+++ Lab1/SkipListLockFree.cs
@@ -49,27 +49,51 @@ namespace Lab1{
             }
         }
 
+        /*
+            Якщо знайдено вузол із таким самим значенням, що й значення, яке потрібно вставити, тоді
+             нічого не слід робити (математичний набір не може містити дублікатів).
+             В іншому випадку ми повинні створити новий вузол і вставити його в список.
+        */
         public bool Insert(Node<T> node){
             var (succs, preds) = GetPredsAndSuccs();
 
             while (true){
+                // шукаємо елемент із таким ж значенням, а також заповнюємо попередників і наступників
                 if (Find(node, ref preds, ref succs)){
                     return false;
                 }
-                
+
+                /*
+                    Ітеруємся по всіх рівнях і заповнюємо всі Next для створеної ноди за допомогою succs
+                */
                 FillInNext(node, succs);
-                
+
+                /*
+                 Наступним кроком є спроба додати новий вузол, зв’язавши його зі списком нижнього рівня між вузлами preds[0] і succs[0], які повертає find().
+                */
                 var pred = preds[Config.MinLevel];
                 var succ = succs[Config.MinLevel];
 
                 node.Next[Config.MinLevel] = new MarkedReference<Node<T>>(succ, false);
 
+                /*
+                 * перевіряємо чи ці Node все ще посилаються один на одного і не були вилучені зі списку
+                 */
                 if (!IsComparedAndExchanged(pred, Config.MinLevel, node, succ)){
                     continue;
                 }
                 
-                IterateOverAllLevelsUp(node, preds, succs);
+                /*
+                 * Для кожного рівня намагаємся з’єднати Node, встановивши попередника, якщо він посилається на дійсного наступника
+                 * У разі успіху викликаємо break і переходить на наступний рівень.
+                 * Якщо це не вдалося, то вузол, на який посилається попередник, повинен бути змінений, і find() викликається знову,
+                 * щоб знайти новий дійсний набір попередників і наступників. Ми відкидаємо результат виклику find(),
+                 * тому що ми дбаємо лише про повторне обчислення попередників і наступників для решти
+                 * незв'язаних рівнів. Після того, як усі рівні пов’язані, метод повертає true
+                 */
                 
+                IterateOverAllLevelsUp(node, preds, succs);
+
                 return true;
             }
         }
@@ -108,25 +132,41 @@ namespace Lab1{
             var (succs, preds) = GetPredsAndSuccs();
 
             while (true){
+                // викликаємо Find щоб перевірити чи існує така Node та шукаємо попередників та наступників
                 if (!Find(node, ref preds, ref succs)){
                     return false;
                 }
-
+                
+                /*
+                 * логічно видаляємо пов’язаний ключ із абстрактного набору та готує його до фізичного видалення
+                 * Ітеруємся починаючи з верхнього рівня і до останнього (не включаючи) і використовуємо CompareAndExchange для Next.
+                 * Якщо виявляється, що посилання позначене (або тому, що воно вже було позначено, або тому, що спроба була успішною),
+                 * метод переходить до посилання наступного рівня.
+                 * В іншому випадку посилання поточного рівня перечитується, оскільки воно повинно бути змінено іншим паралельним потоком, тому спробу позначення потрібно повторити.
+                 */
                 Node<T> succ = null;
                 IterateOverAllLevelDown(ref succ, node);
                 var marked = false;
                 succ = node.Next[Config.MinLevel].Get(ref marked);
-
+                /*
+                 * Власне видалення Node
+                 * позначаємо Next поле за допомогою compareAndSet().
+                 * Перш ніж повернути true, метод find() викликається знову.
+                 * Цей виклик є оптимізацією: як побічний ефект, find() фізично видаляє всі посилання на вузол, який він шукає, якщо цей вузол уже логічно видалено
+                 */
                 return RemoveSucc(succ, node, succs, preds, ref marked);
             }
         }
-        private void OnFind(int level, ref bool marked, ref Node<T> curr, ref Node<T> pred, Node<T> node, bool isRetryNeeded){
+
+        private void OnFind(int level, ref bool marked, ref Node<T> curr, ref Node<T> pred, Node<T> node,
+            bool isRetryNeeded){
             while (true){
                 var succ = curr.Next[level].Get(ref marked);
                 while (marked){
                     if (!IsComparedAndExchanged(pred, level, succ, curr)){
                         throw new IsComparedNotFoundException("Not Found");
                     }
+
                     curr = pred.Next[level].Value;
                     succ = curr.Next[level].Get(ref marked);
                 }
@@ -138,9 +178,10 @@ namespace Lab1{
 
                 else{
                     break;
-                }           
+                }
             }
         }
+
         public bool Find(Node<T> node, ref Node<T>[] preds, ref Node<T>[] succs){
             var marked = false;
             var isRetryNeeded = false;
